9854a169b41ae4e5cd0cd4addf8dfb6e4a8eba70
HSJPRIME <wleo12345@naver.com>
2018-07-13 02:41:31 +0900

    temp

--- board/php/Register_img.php
+++ board/php/Register_img.php
@@ -1,3 +1,8 @@
 <?
-echo "안뇽!";
+// print_r($_FILES['file_arr']);
+print ($_FILES['file_arr']['tmp_name'][0]."러러"."../profile_img/".$_FILES['file_arr']['name'][0]);
+
+$sourcePath = $_FILES['file_arr']['tmp_name'][0];       // Storing source path of the file in a variable
+$targetPath = "../profile_img/".$_FILES['file_arr']['name'][0]; // Target path where file is to be stored
+move_uploaded_file($sourcePath,$targetPath) ;    // Moving Uploaded file
 ?>
